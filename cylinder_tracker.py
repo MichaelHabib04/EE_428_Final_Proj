a8ca237706f6598d2ab529414fac6309fb597200
Antony Chen <122947939+AntonyC03S@users.noreply.github.com>
2026-03-11 10:27:53 -0700

    Cleaning up code

--- cylinder_tracker.py
+++ cylinder_tracker.py
@@ -1,8 +1,47 @@
 import cv2 
 import numpy as np
 
-cap = cv2.VideoCapture('videos/test3_5_2.h264')
+# ------------ Helper Functions -------
+def color_cali(cali_sqare, hsv):
+    x1, y1 = cali_sqare[0]
+    x2, y2 = cali_sqare[1]
 
+    roi_hue = hsv[y1:y2, x1:x2, 0]
+    hue_mean = int(np.round(roi_hue.mean()))
+    hue_mean = max(0, min(179, hue_mean))
+    hsv_pixel = np.uint8([[[hue_mean, 255, 255]]])
+    color_bgr = tuple(int(v) for v in cv2.cvtColor(hsv_pixel, cv2.COLOR_HSV2BGR)[0, 0])
+
+    lower = np.array([hue_mean-threshold,  30, 160], dtype=np.uint8)
+    upper = np.array([hue_mean+threshold, 100,   255], dtype=np.uint8)
+
+    return (lower, upper, color_bgr)
+
+
+def draw_rectan_tracker(frame, lower, upper):
+    blur_frame = cv2.blur(frame, (9, 9), 0)
+    new_hsv = cv2.cvtColor(blur_frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(new_hsv, lower, upper) 
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area < 40: 
+            continue
+        x, y, w, h = cv2.boundingRect(c)
+        if w > 20 :
+            if w/h > 2:
+                continue
+            
+        cv2.rectangle(frame, (x,y),  (x+w,y+h), color_bgr_pink, 2)
+    return frame
+
+
+
+
+# ------------ Main  -------
+
+cap = cv2.VideoCapture('videos/test3_5_2.h264')
 
 if not cap.isOpened():
     print("Error opening video file")
@@ -14,76 +53,20 @@ cali_sqare_2 =[(835,535),(860,548)]
 first_frame = 0
 threshold = 20
 while True:
-    ret, new_frame = cap.read()
-    frame = new_frame
+    ret, frame = cap.read()
     if not ret:
         break 
 
     if first_frame == 0:
-        untocie = frame
-        h, w = frame.shape[:2]
-        frame = cv2.blur(frame, (9, 9), 0)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        x1, y1 = cali_sqare_1[0]
-        x2, y2 = cali_sqare_1[1]
-
-        roi_hue = hsv[y1:y2, x1:x2, 0]
-        hue_mean = int(np.round(roi_hue.mean()))
-        hue_mean = max(0, min(179, hue_mean))
-        hsv_pixel = np.uint8([[[hue_mean, 255, 255]]])
-        color_bgr_1 = tuple(int(v) for v in cv2.cvtColor(hsv_pixel, cv2.COLOR_HSV2BGR)[0, 0])
-
-        lower_pink = np.array([hue_mean-threshold,  30, 160], dtype=np.uint8)
-        upper_pink = np.array([hue_mean+threshold, 100,   255], dtype=np.uint8)
-
-        x1, y1 = cali_sqare_2[0]
-        x2, y2 = cali_sqare_2[1]
-
-        roi_hue = hsv[y1:y2, x1:x2, 0]
-        hue_mean = int(np.round(roi_hue.mean()))
-        hue_mean = max(0, min(179, hue_mean))
-        hsv_pixel = np.uint8([[[hue_mean, 255, 255]]])
-        color_bgr_1 = tuple(int(v) for v in cv2.cvtColor(hsv_pixel, cv2.COLOR_HSV2BGR)[0, 0])
-
-        lower_red = np.array([hue_mean-threshold,  30, 160], dtype=np.uint8)
-        upper_red = np.array([hue_mean+threshold, 100,   255], dtype=np.uint8)
-
+        lower_pink, upper_pink, color_bgr_pink = color_cali(cali_sqare_1, hsv)
+        lower_red, upper_red, color_bgr_red = color_cali(cali_sqare_2, hsv)
         first_frame = 1
 
-    frame = cv2.blur(new_frame, (9, 9), 0)
-    new_hsv = cv2.cvtColor(new_frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(new_hsv, lower_pink, upper_pink) 
+    frame = draw_rectan_tracker(frame, lower_pink, upper_pink)
 
-    result = cv2.bitwise_and(new_frame, new_frame, mask=mask)
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for c in contours:
-        area = cv2.contourArea(c)
-        if area < 40: 
-            continue
-        x, y, w, h = cv2.boundingRect(c)
-        if w > 20 :
-            if w/h > 2:
-                continue
-            
-        cv2.rectangle(new_frame, (x,y),  (x+w,y+h), color_bgr_1, 2)
-
-    # mask = cv2.inRange(new_hsv, lower_red, upper_red) 
-
-    # result = cv2.bitwise_and(new_frame, new_frame, mask=mask)
-    # contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # for c in contours:
-    #     area = cv2.contourArea(c)
-    #     if area < 40: 
-    #         continue
-    #     x, y, w, h = cv2.boundingRect(c)
-    #     if w > 20 :
-    #         if w/h > 2:
-    #             continue
-            
-    #     cv2.rectangle(new_frame, (x,y),  (x+w,y+h), color_bgr_1, 2)
-
-    cv2.imshow('FRAME', new_frame)
+    cv2.imshow('FRAME', frame)
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
